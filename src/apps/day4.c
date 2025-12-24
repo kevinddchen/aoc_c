c36b416b45ab182c6d967ac8adc3f198a39b9a43
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-12-23 22:48:36 -0800

    Refactor day4

--- src/apps/day4.c
+++ src/apps/day4.c
@@ -5,13 +5,15 @@
 
 static const char FILENAME[] = "files/day4.txt";
 
-// Represents offsets between a cell and its 8 neighbors.
+static const int DAY4_PART1_ANS = 1320;
+
+// Represents offset vectors between a cell and its 8 neighbors.
 typedef struct {
     int dcol;
     int drow;
 } Offset;
 
-// All 8 possible offsets.
+// All 8 possible offset vectors to a cell's neighbors.
 static const Offset OFFSETS[] = {
     {-1, -1},
     {0, -1},
@@ -27,25 +29,24 @@ static const int NUM_OFFSETS = sizeof(OFFSETS) / sizeof(OFFSETS[0]);
 // A roll is "accessible" if has fewer than this many neighboring rolls.
 static const int ACCESSIBLE_THRESHOLD = 4;
 
-int main()
+/**
+ * Read the file and obtain grid.
+ */
+void initialize_grid(char** restrict grid_ptr, size_t* restrict cols_ptr, size_t* restrict rows_ptr)
 {
     FILE* fp = fopen(FILENAME, "r");
     assert(fp != NULL);
 
     // figure out dimensions of grid
-    size_t cols = {};
-    size_t rows = {};
-    {
-        cols = 0;
-        while (fgetc(fp) != '\n') {
-            cols++;
-        }
-        rows = 1;
-        int c = {};
-        while ((c = fgetc(fp)) != EOF) {
-            if (c == '\n')
-                rows++;
-        }
+    size_t cols = 0;
+    while (fgetc(fp) != '\n') {
+        cols++;
+    }
+    size_t rows = 1;
+    int c = {};
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == '\n')
+            rows++;
     }
 
     // 2d grid is represented in memory as 1d array. we use row-major ordering.
@@ -53,16 +54,21 @@ int main()
     assert(grid != NULL);
 
     // copy file into grid
-    {
-        fseek(fp, 0, SEEK_SET);  // set file pointer to beginning of file
-        for (size_t row = 0; row < rows; row++) {
-            fread(grid + row * cols, sizeof(char), cols, fp);  // copy row into grid
-            fseek(fp, 1, SEEK_CUR);                            // skip newline char
-        }
+    fseek(fp, 0, SEEK_SET);  // set file pointer to beginning of file
+    for (size_t row = 0; row < rows; row++) {
+        fread(grid + row * cols, sizeof(char), cols, fp);  // copy row into grid
+        fseek(fp, 1, SEEK_CUR);                            // skip newline char
     }
-    fclose(fp);
 
-    int forklift_accessible_rolls = 0;
+    fclose(fp);
+    *cols_ptr = cols;
+    *rows_ptr = rows;
+    *grid_ptr = grid;
+}
+
+int count_accessible_rolls(const char* restrict grid, size_t cols, size_t rows)
+{
+    int accessible_rolls = 0;
 
     // look for all `@` in grid surrounded by fewer than 4 `@`s
     for (int row = 0; row < (int)rows; row++) {
@@ -83,13 +89,27 @@ int main()
                     num_roll_neighbors++;
             }
             if (num_roll_neighbors < ACCESSIBLE_THRESHOLD)
-                forklift_accessible_rolls++;
+                accessible_rolls++;
         }
     }
+    return accessible_rolls;
+}
+
+int main()
+{
+    char* grid = {};
+    size_t cols = {};
+    size_t rows = {};
+    initialize_grid(&grid, &cols, &rows);
+
+    int accessible_rolls = count_accessible_rolls(grid, cols, rows);
+
     free(grid);
 
+    assert(accessible_rolls == DAY4_PART1_ANS);
+
     printf("Day 4\n");
-    printf("Part 1: %d\n", forklift_accessible_rolls);
+    printf("Part 1: %d\n", accessible_rolls);
 
     return EXIT_SUCCESS;
 }
