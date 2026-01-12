55e43ceae98c633d1b76d5c64a7fc9569bf8f46c
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2026-01-11 17:49:52 -0800

    Add day7 part2

--- src/apps/day7.c
+++ src/apps/day7.c
@@ -8,27 +8,68 @@
 static const char FILENAME[] = "files/day7.txt";
 
 static const int DAY7_PART1_ANS = 1560;
+static const long DAY7_PART2_ANS = 25592971184998;
 
-int step_beams(const char* prev_line, char* curr_line, size_t line_length)
+/**
+ * Extend the beams downwards by one line.
+ * @param curr_line Current line.
+ * @param next_line Line below the current line, will be modified by inserting '|' characters.
+ * @param line_length Length of a line, in number of characters.
+ */
+int extend_beams(const char* curr_line, char* next_line, size_t line_length)
 {
     int num_splits = 0;
     for (size_t i = 0; i < line_length; i++) {
-        const char prev_char = prev_line[i];
         const char curr_char = curr_line[i];
-        if (prev_char != 'S' && prev_char != '|')
+        const char next_char = next_line[i];
+        if (curr_char != '|' && curr_char != 'S')
             continue;
         // extend beam
-        if (curr_char == '^') {
+        if (next_char == '^') {
             // split beam
             num_splits++;
-            if (i > 0)
-                curr_line[i - 1] = '|';
-            if (i < line_length - 1)
-                curr_line[i + 1] = '|';
-        } else
-            curr_line[i] = '|';
+            assert(i > 0 && i < line_length - 1);
+            next_line[i - 1] = '|';
+            next_line[i + 1] = '|';
+        } else {
+            // continue beam
+            next_line[i] = '|';
+        }
     }
     return num_splits;
+}
+
+/**
+ * Backtrack up by one line, updating `curr_timelines` to the appropriate values.
+ * @param curr_line Current line.
+ * @param prev_line Line above the current line.
+ * @param curr_timelines Current timelines associated with each character of the current line. Will be updated with the
+ * appropriate values for the previous line.
+ * @param line_length Length of a line, in number of characters.
+ */
+void backtrack(const char* curr_line, const char* prev_line, long* curr_timelines, size_t line_length)
+{
+    long* prev_timelines = calloc(line_length, sizeof(long));
+    assert(prev_timelines != NULL);
+
+    for (size_t i = 0; i < line_length; i++) {
+        const char prev_char = prev_line[i];
+        const char curr_char = curr_line[i];
+        if (prev_char != '|' && prev_char != 'S')
+            continue;
+        if (curr_char == '^') {
+            // add up number of timelines from both sides of the split
+            assert(i > 0 && i < line_length - 1);
+            prev_timelines[i] = curr_timelines[i - 1] + curr_timelines[i + 1];
+        } else {
+            // timeline continues
+            prev_timelines[i] = curr_timelines[i];
+        }
+    }
+    memcpy(curr_timelines, prev_timelines, line_length * sizeof(long));
+
+    free(prev_timelines);
+    prev_timelines = NULL;
 }
 
 int main()
@@ -42,15 +83,48 @@ int main()
     for (size_t i = 1; i < lines.count; i++)
         assert(strlen(((char**)lines.items)[i]) == line_length);
 
+    // forward pass: we simply step through the diagram downwards, updating the characters as we go to indicate the
+    // beams path, and counting the number of splits.
+
     int num_splits = 0;
-    for (size_t i = 1; i < lines.count; i++)
-        num_splits += step_beams(((char**)lines.items)[i - 1], ((char**)lines.items)[i], line_length);
+    for (size_t i = 0; i < lines.count - 1; i++)
+        num_splits += extend_beams(((char**)lines.items)[i], ((char**)lines.items)[i + 1], line_length);
+
+    // backward pass: we use dynamic programming to compute the answer. Starting at the bottom, we track the number of
+    // timelines associated with each beam. At the bottom, each beam corresponds to 1 timeline. As we step upwards, when
+    // we come across a '^' spliter, the number of timelines equals the sum of the number of timelines to the left and
+    // right of the splitter.
+
+    long* timelines = calloc(line_length, sizeof(long));
+    assert(timelines != NULL);
+
+    // initialize values based on last line
+    for (size_t i = 0; i < line_length; i++) {
+        if (((char**)lines.items)[lines.count - 1][i] == '|')
+            timelines[i] = 1;
+    }
+
+    for (size_t i = lines.count - 1; i > 0; i--)
+        backtrack(((char**)lines.items)[i], ((char**)lines.items)[i - 1], timelines, line_length);
+
+    // obtain the answer
+    long num_timelines = -1;
+    for (size_t i = 0; i < line_length; i++) {
+        if (((char**)lines.items)[0][i] == 'S') {
+            num_timelines = timelines[i];
+            break;
+        }
+    }
+    assert(num_timelines != -1);
 
     printf("Day 7\n");
     printf("Part 1: %d\n", num_splits);
+    printf("Part 2: %ld\n", num_timelines);
 
     assert(num_splits == DAY7_PART1_ANS);
+    assert(num_timelines == DAY7_PART2_ANS);
 
+    free(timelines);
     io_free_lines(&lines);
 
     return EXIT_SUCCESS;
